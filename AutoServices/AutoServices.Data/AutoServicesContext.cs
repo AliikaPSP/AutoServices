164cdf03ee6be3fc5bbb2a9ad6ab7649303a4027
Aliika Puusepp <153951390+AliikaPSP@users.noreply.github.com>
2025-01-05 23:17:56 +0200

    Project Setup

---
+++ AutoServices/AutoServices.Data/AutoServicesContext.cs
@@ -0,0 +1,18 @@
+﻿using AutoServices.Core.Domain;
+using Microsoft.EntityFrameworkCore;
+using System.Xml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoServices.Data
+{
+    public class AutoServicesContext : DbContext
+    {
+        public AutoServicesContext(DbContextOptions<AutoServicesContext> options)
+            : base(options) { }
+        public DbSet<Car> Cars { get; set; }
+    }
+}
