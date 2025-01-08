80e15f94e62cceb1917e25651f2439f367e4f2e0
Aliika Puusepp <153951390+AliikaPSP@users.noreply.github.com>
2025-01-08 23:30:17 +0200

    Cars IndexViewModel and Index.cshtml

---
+++ AutoServices/AutoServices/Models/Cars/CarsIndexViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace AutoServices.Models.Cars
+{
+    public class CarsIndexViewModel
+    {
+        public Guid? Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+    }
+}
