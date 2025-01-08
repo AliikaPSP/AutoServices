e270efbc029867a5500421e012b51c03aeb0052e
Aliika Puusepp <153951390+AliikaPSP@users.noreply.github.com>
2025-01-08 23:29:38 +0200

    Cars Controller

---
+++ AutoServices/AutoServices/Controllers/CarsController.cs
@@ -0,0 +1,31 @@
+﻿using AutoServices.Data;
+using AutoServices.Models.Cars;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AutoServices.Controllers
+{
+    public class CarsController : Controller
+    {
+        private readonly AutoServicesContext _context;
+        public CarsController
+            (
+            AutoServicesContext context
+            )
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var result = _context.Cars
+                .Select(x => new CarsIndexViewModel
+                {
+                    Id = x.Id,
+                    Make = x.Make,
+                    Model = x.Model,
+                    Year = x.Year,
+                });
+            return View(result);
+        }
+    }
+}
