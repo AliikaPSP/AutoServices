4bc62b5a40b8572e53240d97b8a697334521c00a
Aliika Puusepp <153951390+AliikaPSP@users.noreply.github.com>
2025-01-10 11:45:27 +0200

    Cars FileToApi

---
+++ AutoServices/AutoServices.ApplicationServices/Services/FileServices.cs
@@ -0,0 +1,51 @@
+﻿using AutoServices.Core.Domain;
+using AutoServices.Core.Dto;
+using AutoServices.Data;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoServices.ApplicationServices.Services
+{
+    public class FileServices
+    {
+        private readonly IHostEnvironment _webHost;
+        private readonly AutoServicesContext _context;
+        public FileServices
+            (
+                IHostEnvironment webHost,
+                AutoServicesContext context
+            )
+        {
+            _webHost = webHost;
+            _context = context;
+        }
+        public void FilesToApi(CarDto dto, Car car)
+        {
+            if (!Directory.Exists(_webHost.ContentRootPath + "\\multipleFileUpload\\"))
+            {
+                Directory.CreateDirectory(_webHost.ContentRootPath + "\\multipleFileUpload\\");
+            }
+            foreach (var file in dto.Files)
+            {
+                string uploadsFolder = Path.Combine(_webHost.ContentRootPath, "multipleFileUpload");
+                string uniqueFileName = Guid.NewGuid().ToString() + "_" + file.FileName;
+                string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    file.CopyTo(fileStream);
+                    FileToApi path = new FileToApi
+                    {
+                        Id = Guid.NewGuid(),
+                        ExistingFilePath = uniqueFileName,
+                        CarId = car.Id
+                    };
+                    _context.FileToApis.AddAsync(path);
+                }
+            }
+        }
+    }
+}
